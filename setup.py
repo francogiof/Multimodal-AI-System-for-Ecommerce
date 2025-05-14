b678ff9f6fd220672ef8befb9d093f28770f9f85
francogionardo <abel.ccapa@tecsup.edu.pe>
2025-05-14 04:02:53 +0000

    Dev Container setup.py - git

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="anyoneai-project",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # requirements will be read from requirements.txt
+    ],
+) 
